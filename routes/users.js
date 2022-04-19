beac9d71597ec4748178909c753ae4c0b8de737a
Nathaniel Johnston <j.johnston.tech@gmail.com>
2022-04-19 15:00:32 -0600

    Add Tuesday's Work to Users Route

--- routes/users.js
+++ routes/users.js
@@ -20,16 +20,25 @@ router.route('/')
   })
   .post((req,res) => {
     db
-      .insert(req.body)
+      .insert({
+        username: req.body.username,
+        rank: req.body.rank,
+        name: req.body.name,
+        crew_id: req.body.crew_id,
+        is_auth: req.body.is_auth,
+        is_trainer: req.body.is_trainer,
+        is_approver: req.body.is_approver
+      })
       .returning('*')
       .into('users')
-      .then((data) => res.status(201).json(data))
-      .catch(err =>
+      .then((data) => res.status(201).json(data[0]))
+      .catch(err => {
+        console.log(err);
         res.status(404).json({
           message:
             'Could not POST users data.'
         })
-      );
+      });
   })
 
 router.route('/:id')
@@ -50,7 +59,7 @@ router.route('/:id')
     db
       .select('*')
       .from('users')
-      .where('id', '=', eq.params.id)
+      .where('id', '=', req.params.id)
       .update({
         id: req.body.id,
         username: req.body.username,
@@ -77,7 +86,7 @@ router.route('/:id')
       .where('id', '=', req.params.id)
       .update(req.body)
       .returning('*')
-      .then((data) => res.status(204).json(data[0]))
+      .then((data) => res.status(201).send("Successfully updated record."))
       .catch(err =>
         res.status(404).json({
           message:
@@ -105,7 +114,77 @@ router.route('/:id')
       db.select('*').from('modules_users').where('user_id', '=', req.params.id)
         .then((data) => res.status(200).json(data))
         .catch(err => res.status(404).json({message: 'Could not GET user modules.'}));
-    });
+    })
+    .post((req, res) => {
+      db
+      .insert(req.body)
+      .returning('*')
+      .into('modules_users')
+      .then((data) => res.status(201).json(data))
+      .catch(err =>
+        res.status(404).json({
+          message:
+            'Could not POST user module.'
+        })
+      );
+    })
+
+  router.route('/:id/modules/:m_id')
+    .delete((req,res) => {
+      db
+        .select('*')
+        .from('modules_users')
+        .where('user_id', '=', req.params.id)
+        .andWhere('module_id', '=', req.params.m_id)
+        .delete()
+        .then((data) => res.status(200).send("Module unassigned from user."))
+        .catch(err =>
+          res.status(404).json({
+            message:
+              'Could not DELETE user module.'
+          })
+        );
+    })
+    .put((req,res) => {
+      db
+        .select('*')
+        .from('modules_users')
+        .where('user_id', '=', req.params.id)
+        .andWhere('module_id', '=', req.params.m_id)
+        .update({
+          module_id: req.body.module_id,
+          user_id: req.body.user_id,
+          is_started: req.body.is_started ?? false,
+          is_completed: req.body.is_completed ?? false,
+          deadline: req.body.deadline ?? null
+          })
+        .returning('*')
+        .then((data) => res.status(201).send("User's module assignment updated."))
+        .catch(err =>
+          res.status(404).json({
+            message:
+              'Could not PUT user module assignment.'
+          })
+        );
+      })
+    .patch((req,res) => {
+      db
+        .select('*')
+        .from('modules_users')
+        .where('user_id', '=', req.params.id)
+        .andWhere('module_id', '=', req.params.m_id)
+        .update(req.body)
+        .returning('*')
+        .then((data) => res.status(201).send("User's module assignment updated."))
+        .catch(err =>
+          res.status(404).json({
+            message:
+              'Could not PATCH user data.'
+          })
+        );
+    })
+
+
 
   router.route('/account/:username')
   .get((req,res) => {
@@ -143,22 +222,32 @@ router.route('/:id')
   router.route('/account/:username/overview')
     .get((req, res) => {
       let p1 = db.select('*').from('users').where('username', '=', `${req.params.username}`);
-      let p2 = db.select('*').from('crews').innerJoin('users', function() {
+      let p2 = db.select('crews.id', 'crews.name').from('crews').innerJoin('users', function() {
         this.on('users.username', '=', db.raw('?', [`${req.params.username}`])).andOn('users.crew_id', '=', 'crews.id');
       });
       let p3 = db.select('*').from('modules_users').innerJoin('users', function() {
         this.on('users.username', '=', db.raw('?', [`${req.params.username}`])).andOn('modules_users.user_id', '=', 'users.id');
       });
-      Promise.all([p1, p2, p3])
-      .then((data) => res.status(200).json(data))
+      let p4 = db.select('*').from('tasks_users').innerJoin('users', function() {
+        this.on('users.username', '=', db.raw('?', [`${req.params.username}`])).andOn('tasks_users.user_id', '=', 'users.id');
+      });
+      Promise.all([p1, p2, p3, p4])
+      .then((dataSet) => {
+        const finalData = {};
+        finalData.userInfo = dataSet[0][0];
+        finalData.crewInfo = dataSet[1][0];
+        finalData.modules = dataSet[2];
+        finalData.tasks = dataSet[3];
+        res.status(200).json(finalData);
+      })
       .catch(err => {
         console.log(err);
           res.status(404).json({
             message:
-              'Could not GET user data.'
+              'Could not GET user overview.'
           });
         }
       );
-    })
+    });
 
 module.exports = router;
